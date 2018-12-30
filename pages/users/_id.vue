8344abe12008993aa9220616c98621e1639b5bf2
tung776 <thanhtung776@gmail.com>
2018-12-30 09:00:52 +0700

    access params and validate

--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -3,3 +3,10 @@
         <h2>user's id {{$route.params.id}}</h2>
     </div>
 </template>
+<script>
+export default {
+    validate (context) {
+        return /^\d+/.test(context.params.id)//Chỉ chấp nhận biến id là kiểu số
+    }
+}
+</script>
