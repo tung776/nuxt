2f565c7d3771b886bddc1c54189cc5cd8d7de0f3
tung776 <thanhtung776@gmail.com>
2018-12-30 10:05:44 +0700

    nuxt-link

--- components/Nav.vue
+++ components/Nav.vue
@@ -15,12 +15,10 @@
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item active">
-          <a class="nav-link" href="/">
-            Home
-          </a>
+            <nuxt-link class="nav-link" to="/">Home</nuxt-link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/users">Users</a>
+            <nuxt-link class="nav-link" to="/users">Users</nuxt-link>
         </li>       
       </ul>
     </div>
